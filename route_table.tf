698e5874a7f0ed5e6a16e77c8d3d5416c53b0ef0
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-19 14:24:16 +0000

    Adding 2ndary private subnets

--- route_table.tf
+++ route_table.tf
@@ -39,6 +39,46 @@ resource "aws_route_table_association" "private_rt_assoc" {
 }
 
 #--------------------------------------------------------------
+# Routing table for Secondary private subnets
+#--------------------------------------------------------------
+resource "aws_route_table" "secondary_private_route_table" {
+  count            = "${length(split(",", var.secondary_private_subnets))}"
+  vpc_id           = "${aws_vpc.main.id}"
+  propagating_vgws = ["${compact(concat(split(",", var.vpc_propagating_vgws), list(aws_vpn_gateway.main_vgw.id)))}"]
+
+  tags {
+    Name          = "${var.environment}.rt.secondary.private.${element(split(",", var.availability_zones), count.index)}"
+    Environment   = "${var.environment}"
+    Description   = "secondary.private.${element(split(",", var.availability_zones), count.index)}"
+    Contact       = "${var.vpc_contact}"
+    Orchestration = "${var.global_orchestration}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#--------------------------------------------------------------
+# Route for private routing table
+#--------------------------------------------------------------
+resource "aws_route" "secondary_private_route" {
+  count                  = "${var.nat_gateway_enabled > 0 ? (length(split(",", var.secondary_private_subnets))): 0}"
+  route_table_id         = "${element(aws_route_table.secondary_private_route_table.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${element(aws_nat_gateway.nat.*.id, count.index)}"
+}
+
+#--------------------------------------------------------------
+# Association between private subnets and route tables
+#--------------------------------------------------------------
+resource "aws_route_table_association" "secondary_private_rt_assoc" {
+  count          = "${length(split(",", var.secondary_private_subnets))}"
+  subnet_id      = "${element(aws_subnet.secondary_private_subnets.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.secondary_private_route_table.*.id, count.index)}"
+}
+
+#--------------------------------------------------------------
 # Routing table for public subnets
 #--------------------------------------------------------------
 resource "aws_route_table" "public_route_table" {
@@ -81,9 +121,9 @@ resource "aws_route_table" "data_route_table" {
   vpc_id = "${aws_vpc.main.id}"
 
   tags {
-    Name          = "${var.environment}.rt.public.${element(split(",", var.availability_zones), count.index)}"
+    Name          = "${var.environment}.rt.data.${element(split(",", var.availability_zones), count.index)}"
     Environment   = "${var.environment}"
-    Description   = "public.${element(split(",", var.availability_zones), count.index)}"
+    Description   = "data.${element(split(",", var.availability_zones), count.index)}"
     Contact       = "${var.vpc_contact}"
     Orchestration = "${var.global_orchestration}"
   }
