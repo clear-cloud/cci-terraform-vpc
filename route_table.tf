985d9e3d10752038800047d6049df6481eaf5a68
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-19 15:20:32 +0000

    adding conditional for 2ndary subnet

--- route_table.tf
+++ route_table.tf
@@ -63,7 +63,7 @@ resource "aws_route_table" "secondary_private_route_table" {
 # Route for Secondary private routing table
 #--------------------------------------------------------------
 resource "aws_route" "secondary_private_route" {
-  count                  = "${var.nat_gateway_enabled > 0 ? (length(split(",", var.secondary_private_subnets))): 0}"
+  count                  = "${var.secondary_private_subnets > 0 ? (length(split(",", var.secondary_private_subnets))): 0}"
   route_table_id         = "${element(aws_route_table.secondary_private_route_table.*.id, count.index)}"
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = "${element(aws_nat_gateway.nat.*.id, count.index)}"
@@ -73,7 +73,7 @@ resource "aws_route" "secondary_private_route" {
 # Association between Secondary private subnets and route tables
 #--------------------------------------------------------------
 resource "aws_route_table_association" "secondary_private_rt_assoc" {
-  count          = "${length(split(",", var.secondary_private_subnets))}"
+  count          = "${var.secondary_private_subnets > 0 ? (length(split(",", var.secondary_private_subnets))): 0}"
   subnet_id      = "${element(aws_subnet.secondary_private_subnets.*.id, count.index)}"
   route_table_id = "${element(aws_route_table.secondary_private_route_table.*.id, count.index)}"
 }
