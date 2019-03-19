fddcdf22f433230d05e446374dcf238fbad68520
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-19 15:59:09 +0000

    Route 53 internal change

--- dns.tf
+++ dns.tf
@@ -1,16 +1,20 @@
-resource "aws_route53_zone" "vpc_public_zone" {
+resource "aws_route53_zone" "route53_zone" {
+  
   name = "${var.dns_domain}"
+  vpc  {
+     vpc_id = "${aws_vpc.main.id}"
+  }
 
   tags {
-    Name          = "${var.environment}.PublicZone"
+    Name          = "${var.environment}.R53_Zone"
     Environment   = "${var.environment}"
-    Description   = "PublicZone"
+    Description   = "Route53Zone"
     Contact       = "${var.vpc_contact}"
     Orchestration = "${var.global_orchestration}"
   }
 }
 
-resource "aws_vpc_dhcp_options" "tio_options" {
+resource "aws_vpc_dhcp_options" "dhcp_options" {
   domain_name         = "${var.dns_domain}"
   domain_name_servers = ["${split(",",var.domain_name_servers)}"]
 
@@ -25,5 +29,5 @@ resource "aws_vpc_dhcp_options" "tio_options" {
 
 resource "aws_vpc_dhcp_options_association" "local_resolver" {
   vpc_id          = "${aws_vpc.main.id}"
-  dhcp_options_id = "${aws_vpc_dhcp_options.tio_options.id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.dhcp_options.id}"
 }
